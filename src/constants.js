ce9f12b5d82464509c62bd68d1ac6bcc11f1b712
xsmming <jing.mi@northeastern.edu>
2020-10-10 14:45:06 -0400

    N2YO updated base API

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const NY20_BASE_URL = "https://www.n2yo.com/rest/v1/satellite";
+export const NY20_BASE_URL = "https://api.n2yo.com/rest/v1/satellite";
 
 export const NY20_API_KEY = "TSPJRE-KXUWXJ-WM67Y2-4K0T";
 
