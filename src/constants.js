b42e633a6ae3928acc6674a4fc915f633a2c8420
xsmming <jing.mi@northeastern.edu>
2020-10-22 22:18:46 -0400

    https issue closed for no enough time

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const NY20_BASE_URL = "https://api.n2yo.com/rest/v1/satellite";
 
 export const NY20_API_KEY = "TSPJRE-KXUWXJ-WM67Y2-4K0T";
-export const CORS_BASE = "https://39257abeeb5e.ngrok.io/jupiter";
+export const CORS_BASE = "http://3.137.145.153/jupiter";
 //https://39257abeeb5e.ngrok.io/jupiter
 //https://3.137.145.153/jupiter
 export const SAT_CATEGORY = "52";
